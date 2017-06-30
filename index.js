86c11751f97f8f1e9dda14e53e32d8619d3fa50e
Amy Cheng <amylynncheng@gmail.com>
2017-06-30 16:28:01 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Imports the Google Cloud client library
+const Language = require('@google-cloud/language');
+
+// Your Google Cloud Platform project ID
+const projectId = 'YOUR_PROJECT_ID';
+
+// Instantiates a client
+const language = Language({
+  projectId: projectId
+});
+
+// The text to analyze
+const text = 'Hello, world!';
+
+// Detects the sentiment of the text
+language.detectSentiment(text)
+  .then((results) => {
+    const sentiment = results[0];
+
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
